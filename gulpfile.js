f7324331ce5147522b2380b3ec56e8b2e2b7dc5b
unknown <you@example.com>
2020-12-20 22:48:09 +0200

    added start

--- gulpfile.js
+++ gulpfile.js
@@ -18,12 +18,12 @@ gulp.task('scripts', function(){
 gulp.task('style', function(){
 	return gulp.src(['vendor/normalize.css', 'vendor/slick/slick.css', ])
 			.pipe(concat('libs.css'))
-			.pipe()
+			.pipe(cssmin())
 			.pipe(gulp.dest('app/css'))
 });
 
 gulp.task('sass', function(){
-	return	gulp.src('app/scss/style.scss')
+	return	gulp.src('app/scss/**/*.scss')
 					.pipe(sass({outputStyle: 'compressed'}))
 					.pipe(rename({suffix : '.min'}))
 					.pipe(autoprefixer({
@@ -32,7 +32,6 @@ gulp.task('sass', function(){
 					.pipe(gulp.dest('app/css'))
 					.pipe(browserSync.reload({stream: true}))
 });
-
 
 
 gulp.task('html', function(){
@@ -54,9 +53,9 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function(){
-	gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+	gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
 	gulp.watch('app/*.html', gulp.parallel('html'))
 	gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
 
-gulp.task('default', gulp.parallel('scripts', 'sass', 'watch', 'browser-sync'))
+gulp.task('default', gulp.parallel('style','scripts', 'sass', 'watch', 'browser-sync'))
